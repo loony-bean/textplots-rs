f219f318991c6bb90b83f120eb8e3b9bd304b1bc
Alexey Suslov <alexey.suslov@sizmek.com>
2018-05-20 11:43:54 +0200

    Up docs

--- examples/plot.rs
+++ examples/plot.rs
@@ -13,11 +13,10 @@ fn main() {
 
     // Default viewport size is 120 x 60 points, with X values ranging from -10 to 10.
     println!("\ny = ln(x)");
-    Chart::default().lineplot(|x| x.ln() ).display();
+    Chart::default().lineplot( f32::ln ).display();
 
     // You can plot several functions on the same chart.
     // However the resolution of text displays is low, and the result might not be great.
-    // (note how you can use `new` to override the defaults)
     println!("\ny = cos(x), y = sin(x) / 2");
     Chart::new(180, 60, -5.0, 5.0)
         .lineplot( |x| x.cos() )
