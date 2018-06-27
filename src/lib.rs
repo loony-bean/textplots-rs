9de9e2bf8dd245a4dc7e690b761b956e4ede171d
Alexey Suslov <alexey.suslov@sizmek.com>
2018-06-27 23:03:52 +0300

    WIP

--- src/lib.rs
+++ src/lib.rs
@@ -69,10 +69,55 @@ pub struct Chart {
     canvas: BrailleCanvas,
 }
 
+#[derive(Debug)]
+pub struct Scale {
+    // domain
+    min_d: f32,
+    max_d: f32,
+    // range
+    min_r: f32,
+    max_r: f32,
+}
+
+impl Scale {
+    pub fn linear(&self, x: f32) -> f32 {
+        let p = (x - self.min_d) / (self.max_d - self.min_d);
+        let r = self.min_r + p * (self.max_r - self.min_r);
+        let r = r.max(self.min_r);
+        let r = r.min(self.max_r);
+        r
+    }
+
+    pub fn inv_linear(&self, i: f32) -> f32 {
+        let p = (i - self.min_r) / (self.max_r - self.min_r);
+        let d = self.min_d + p * (self.max_d - self.min_d);
+
+        let d = d.max(self.min_d);
+        let d = d.min(self.max_d);
+        d
+    }
+
+    pub fn new(min_d: f32, max_d: f32, min_r: f32, max_r: f32) -> Self {
+        Scale {
+            min_d,
+            max_d,
+            min_r,
+            max_r,
+        }
+    }
+}
+
+pub enum Shape<'a> {
+    Continuous(fn(f32) -> f32),
+    Lines(&'a [(f32, f32)]),
+    Steps(&'a [(f32, f32)]),
+    Bars(&'a [(f32, f32)]),
+}
+
 /// Provides an interface for drawing plots.
 pub trait Plot {
     /// Draws a [line chart](https://en.wikipedia.org/wiki/Line_chart) of points connected by straight line segments.
-    fn lineplot(&mut self, func: impl Fn(f32) -> f32) -> &mut Chart;
+    fn lineplot(&mut self, shape: Shape) -> &mut Chart;
 }
 
 impl Default for Chart {
@@ -99,8 +144,8 @@ impl Chart {
         Self {
             xmin,
             xmax,
-            ymin: 0.0,
-            ymax: 0.0,
+            ymin: 10.0,
+            ymax: -10.0,
             width,
             height,
             canvas: BrailleCanvas::new(width, height),
@@ -112,16 +157,16 @@ impl Chart {
         let w = self.width;
         let h = self.height;
 
-        self.canvas.line(0, 0, 0, h);
-        self.canvas.line(0, 0, w, 0);
-        self.canvas.line(0, h, w, h);
-        self.canvas.line(w, 0, w, h);
+        self.vline(0);
+        self.vline(w);
+        self.hline(0);
+        self.hline(h);
     }
 
     /// Draws vertical line,
     fn vline(&mut self, i: u32) {
-        if i > 0 && i < self.width {
-            for j in 0..self.height {
+        if i <= self.width {
+            for j in 0..=self.height {
                 if j % 3 == 0 {
                     self.canvas.set(i, j);
                 }
@@ -131,8 +176,8 @@ impl Chart {
 
     /// Draws horisontal line.
     fn hline(&mut self, j: u32) {
-        if j > 0 && j < self.height {
-            for i in 0..self.width {
+        if j <= self.height {
+            for i in 0..=self.width {
                 if i % 3 == 0 {
                     self.canvas.set(i, self.height - j);
                 }
@@ -159,57 +204,97 @@ impl Chart {
 }
 
 impl Plot for Chart {
-    fn lineplot(&mut self, func: impl Fn(f32) -> f32) -> &mut Chart {
+    fn lineplot(&mut self, shape: Shape) -> &mut Chart {
         self.borders();
 
-        // calculation of x range
-        let xrange = (self.xmax - self.xmin).abs();
-        let xstep = xrange / self.width as f32;
+        let x_scale = Scale::new(self.xmin, self.xmax, 0.0, self.width as f32);
 
-        // auto calculation of y range
-        let ys: Vec<_> = (0..self.width)
-            .into_iter()
-            .map(|i| func(self.xmin + (i as f32) * xstep) )
-            .collect();
+        let ys: Vec<_> = match shape {
+            Shape::Continuous(f) => {
+                (0..self.width)
+                .into_iter()
+                .map(|i| f(x_scale.inv_linear(i as f32)) )
+                .collect()
+            },
+            | Shape::Lines(dt)
+            | Shape::Steps(dt)
+            | Shape::Bars(dt) => {
+                dt.iter().map(|(_x, y)| *y).collect()
+            },
+        };
 
-        let mut ymax = *ys.iter().max_by( |x, y| x.partial_cmp(y).unwrap_or(cmp::Ordering::Equal) ).unwrap_or(&0.0);
-        let mut ymin = *ys.iter().min_by( |x, y| x.partial_cmp(y).unwrap_or(cmp::Ordering::Equal) ).unwrap_or(&0.0);
+        let ymax = *ys.iter().max_by( |x, y| x.partial_cmp(y).unwrap_or(cmp::Ordering::Equal) ).unwrap_or(&0.0);
+        let ymin = *ys.iter().min_by( |x, y| x.partial_cmp(y).unwrap_or(cmp::Ordering::Equal) ).unwrap_or(&0.0);
 
         self.ymin = f32::min(self.ymin, ymin);
         self.ymax = f32::max(self.ymax, ymax);
 
-        let margin = (self.ymax - self.ymin) * 0.05;
-        ymin = self.ymin - margin;
-        ymax = self.ymax + margin;
-        let yrange = ymax - ymin;
+        let y_scale = Scale::new(self.ymin, self.ymax, 0.0, self.height as f32);
 
         // show axis
-        let i_center = ((xrange - self.xmax) / xrange) * self.width as f32;
-        self.vline(i_center as u32);
-
-        let j_center = ((yrange - ymax) / yrange) * self.height as f32;
-        self.hline(j_center as u32);
+        self.vline(x_scale.linear(0.0) as u32);
+        self.hline(y_scale.linear(0.0) as u32);
 
         // calculate func and translate (x, y) points into screen coordinates
-        let points: Vec<_> = (0..self.width)
-            .into_iter()
-            .filter_map(|i| {
-                let x = self.xmin + (i as f32) * xstep;
-                let y = func(x);
-                if y.is_normal() {
-                    let j = (((y - ymin) / yrange) * self.height as f32) as i32;
-                    let j = j.max(0) as u32;
-                    let j = j.min(self.width);
-                    Some((i, self.height - j))
-                } else {
-                    None
-                }
-            }).collect();
+        let points: Vec<_> = match shape {
+            Shape::Continuous(f) => {
+                (0..self.width)
+                .into_iter()
+                .filter_map(|i| {
+                    let x = x_scale.inv_linear(i as f32);
+                    // println!("x = {}", x);
+                    let y = f(x);
+                    // println!("y = {}", y);
+                    if y.is_normal() {
+                        let j = y_scale.linear(y);
+                        // println!("j = {}", j);
+                        Some((i, self.height - j as u32))
+                    } else {
+                        None
+                    }
+                }).collect()
+            },
+            | Shape::Lines(dt)
+            | Shape::Steps(dt)
+            | Shape::Bars(dt) => {
+                dt
+                .iter()
+                .filter_map(|(x, y)| {
+                    let i = x_scale.linear(*x) as u32;
+                    let j = y_scale.linear(*y) as u32;
+                    if i <= self.width && j <= self.height {
+                        Some( (i, self.height - j) )
+                    } else {
+                        None
+                    }
+                })
+                .collect()
+            },
+        };
 
         for pair in points.windows(2) {
             let (x1, y1) = pair[0];
             let (x2, y2) = pair[1];
-            self.canvas.line(x1, y1, x2, y2);
+
+            match shape {
+                Shape::Continuous(_) => {
+                    self.canvas.line(x1, y1, x2, y2);
+                },
+                Shape::Lines(_) => {
+                    self.canvas.line(x1, y1, x2, y2);
+                },
+                Shape::Steps(_) => {
+                    self.canvas.line(x1, y2, x2, y2);
+                    self.canvas.line(x1, y1, x1, y2);
+                },
+                Shape::Bars(_) => {
+                    self.canvas.line(x1, y2, x2, y2);
+                    self.canvas.line(x1, y1, x1, y2);
+
+                    self.canvas.line(x1, self.height, x1, y1);
+                    self.canvas.line(x2, self.height, x2, y2);
+                },
+            }
         }
 
         self
