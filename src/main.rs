f0576022394eca2424f686e973e314b872ef7c30
Alexey Suslov <alexey.suslov@gmail.com>
2021-04-04 12:47:02 +0300

    Update readme

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,30 @@
-use textplots::{Chart, Plot, Shape};
-use clap::{Arg, App};
+use structopt::StructOpt;
 use std::process::exit;
+use textplots::{Chart, Plot, Shape};
+
+#[derive(StructOpt)]
+struct Opt {
+    /// Formula to plot
+    #[structopt(name = "FORMULA")]
+    formula: String,
+    /// X-axis start value.
+    #[structopt(long, default_value = "-10.0")]
+    xmin: f32,
+    /// X-axis end value.
+    #[structopt(long, default_value = "10.0")]
+    xmax: f32,
+    /// Canvas width in points.
+    #[structopt(short, long, default_value = "180")]
+    width: u32,
+    /// Canvas height in points.
+    #[structopt(short, long, default_value = "60")]
+    height: u32,
+}
 
 fn main() {
-    let matches = App::new(env!("CARGO_BIN_NAME"))
-      .version(env!("CARGO_PKG_VERSION"))
-      .author(env!("CARGO_PKG_AUTHORS"))
-      .about(env!("CARGO_PKG_DESCRIPTION"))
-      .arg(Arg::with_name("FORMULA")
-           .help("Formula to plot")
-           .required(true)
-           .index(1))
-      .get_matches();
+    let opt = Opt::from_args();
 
-    let formula = matches.value_of("FORMULA").unwrap(); // unreachable unwrap
-    let res = formula.parse().and_then(|expr: meval::Expr| expr.bind("x"));
+    let res = opt.formula.parse().and_then(|expr: meval::Expr| expr.bind("x"));
     let func = match res {
         Ok(func) => func,
         Err(err) => {
@@ -23,11 +33,11 @@ fn main() {
 
             eprintln!("{}", err);
             exit(1);
-        },
+        }
     };
 
-    println!("y = {}", formula);
-    Chart::default()
-    	.lineplot(&Shape::Continuous(Box::new(|x| func(x.into()) as f32)))
-    	.display();
+    println!("y = {}", opt.formula);
+    Chart::new(opt.width, opt.height, opt.xmin, opt.xmax)
+        .lineplot(&Shape::Continuous(Box::new(|x| func(x.into()) as f32)))
+        .display();
 }
